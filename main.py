512998ee7cd86f049e7c71d3c5a17fdf5e0ab6b0
Rpi4B <1075671577@qq.com>
2023-11-11 17:56:37 +0800

    water sensor basic code finished

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import multiprocessing as mp
-from multiprocessing import Manager
+import smbus
 
 def init_ultrasonic_sensor(trig, echo):
     GPIO.setmode(GPIO.BCM)
@@ -47,49 +47,73 @@ def ultrasonic_sensor_process(trig1, trig2 , echo1, echo2, distance_value):
         distance1 = us_get_distance(trig1, echo1)
         distance2 = us_get_distance(trig2, echo2)
         distance_value.value = min(distance1, distance2)
-        print(distance_value.value)
+        #print(distance_value.value)
         time.sleep(0.2)
 
-def buzzer_trigger_process(trig, distance_value):
+def water_sensor_process(address, bus_id, channel, water_voltage_threshold, in_water):
+    while True:
+        bus = smbus.SMBus(bus_id)
+        water_detect_voltage = bus.read_byte(address) * 5 / 255
+        if water_detect_voltage < water_voltage_threshold:
+            in_water.value = False
+        else:
+            in_water.value = True
+        print(in_water.value)
+        time.sleep(0.5)
+
+def buzzer_trigger_process(trig, distance_value, in_water):
     init_buzzer(trig)
     while True:
-        distance = distance_value.value
-        if 40 < distance < 50:
+        if in_water.value == True:
             GPIO.output(trig, GPIO.HIGH)
-            time.sleep(0.1)
-            GPIO.output(trig, GPIO.LOW)
             time.sleep(0.5)
-        elif 30 <distance <= 40:
-            GPIO.output(trig, GPIO.HIGH)
-            time.sleep(0.1)
-            GPIO.output(trig, GPIO.LOW)
-            time.sleep(0.4)
-        elif 20 <distance <= 30:
-            GPIO.output(trig, GPIO.HIGH)
-            time.sleep(0.1)
-            GPIO.output(trig, GPIO.LOW)
-            time.sleep(0.2)
-        elif distance <= 20:
-            GPIO.output(trig, GPIO.HIGH)
-            time.sleep(0.1)
-            GPIO.output(trig, GPIO.LOW)
-            time.sleep(0.1)
         else:
-            GPIO.output(trig, GPIO.LOW)
-            time.sleep(0.2)
+            distance = distance_value.value
+            if 40 < distance < 50:
+                GPIO.output(trig, GPIO.HIGH)
+                time.sleep(0.1)
+                GPIO.output(trig, GPIO.LOW)
+                time.sleep(0.5)
+            elif 30 <distance <= 40:
+                GPIO.output(trig, GPIO.HIGH)
+                time.sleep(0.1)
+                GPIO.output(trig, GPIO.LOW)
+                time.sleep(0.4)
+            elif 20 <distance <= 30:
+                GPIO.output(trig, GPIO.HIGH)
+                time.sleep(0.1)
+                GPIO.output(trig, GPIO.LOW)
+                time.sleep(0.2)
+            elif distance <= 20:
+                GPIO.output(trig, GPIO.HIGH)
+                time.sleep(0.1)
+                GPIO.output(trig, GPIO.LOW)
+                time.sleep(0.1)
+            else:
+                GPIO.output(trig, GPIO.LOW)
+                time.sleep(0.2)
 
 if __name__ == "__main__":
     ustrig = [14,23]
     usecho = [15,24]
     bztrig = 25
 
+    address = 0x48
+    bus_id = 1
+    channel = 0x00
+    water_voltage_threshold = 2.5
+    
     distance_value = mp.Value("f",0)
+    in_water = mp.Value("i", False)
+    
 
     us_process = mp.Process(target=ultrasonic_sensor_process, args=(ustrig[0], ustrig[1], usecho[0], usecho[1], distance_value))
-    bz_process = mp.Process(target=buzzer_trigger_process, args=(bztrig, distance_value))
+    bz_process = mp.Process(target=buzzer_trigger_process, args=(bztrig, distance_value, in_water))
+    ws_process = mp.Process(target=water_sensor_process, args=(address, bus_id, channel, water_voltage_threshold, in_water))
 
     us_process.start()
     bz_process.start()
+    ws_process.start()
 
     try:
         while True:
@@ -98,6 +122,9 @@ if __name__ == "__main__":
         us_process.terminate()
         us_process.join()
 
-        bz_process.terminate()
         bz_process.join()
+        
+        ws_process.terminate()
+        ws_process.join()
+        
         GPIO.cleanup()
